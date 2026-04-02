f3cea4dcfea0d1466be5855649fa9e666a1517c4
ASteve29 <29hyperloop@gmail.com>
2026-04-02 17:57:06 +0100

    Implemented configurable multi-pheromone diffusion system

--- entities.py
+++ entities.py
@@ -1,5 +1,26 @@
 import random
 
+pheromone_settings = {
+    "home": {
+        "diffusion": 0.2,
+        "evaporation": 0.99999
+    },
+    "food": {
+        "diffusion": 0.15,
+        "evaporation": 0.999
+    },
+    "forager ant": {
+        "diffusion": 0.05,
+        "evaporation": 0.98
+    },
+    "scout ant": {
+        "diffusion": 0.05,
+        "evaporation": 0.98
+    }
+}
+
+# Classes
+
 class Tile:
     def __init__(self):
         self.food = 0
@@ -7,7 +28,8 @@ class Tile:
         self.pheromones = {
             "home": 0,
             "food": 0,
-            "ant": 0
+            "forager ant": 0,
+            "scout ant": 0
         }
 
 class Ant:
@@ -21,22 +43,26 @@ class Ant:
         self.y = max(0, min(height - 1, self.y + random.randint(-1, 1)))
         self.health -= 1
 
+# Functions
+
 def diffuse_pheremones(grid, width, height):
-    current_scents = [[grid[y][x].pheromones["home"] for x in range(width)] for y in range(height)]
-
-    for y in range(height):
-        for x in range(width):
-
-            neighbor_sum = 0
-            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < width and 0 <= ny < height:
-                    neighbor_sum += current_scents[ny][nx]
-            
-            diff_factor = 0.2  # Lower is smoother
-            evap_rate = 0.99999  # Lower = faster fading
-            
-            center_scent = current_scents[y][x]
-            diffused = (center_scent * (1 - 4 * diff_factor)) + (neighbor_sum * diff_factor)
-            
-            grid[y][x].pheromones["home"] = diffused * evap_rate
+    for scents in phermemone_settings:
+    	current_scents = [[grid[y][x].pheromones[scents] for x in range(width)] for y in range(height)]
+	
+	    for y in range(height):
+	        for x in range(width):
+	
+	            neighbor_sum = 0
+	            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+	                nx, ny = x + dx, y + dy
+	                if 0 <= nx < width and 0 <= ny < height:
+	                    neighbor_sum += current_scents[ny][nx]
+	            
+	            diff_factor = pheromone_settings[scents]["diffusion"]
+	            evap_rate = pheromone_settings[scents]["evaporation"]
+	            
+	            
+	            center_scent = current_scents[y][x]
+	            diffused = (center_scent * (1 - 4 * diff_factor)) + (neighbor_sum * diff_factor)
+	            
+	            grid[y][x].pheromones[scents] = diffused * evap_rate
